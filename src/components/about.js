2e3a7556e16d6ed69a3b9f18b7f512711d5dd903
Lance Hietpas <lancehietpas@gmail.com>
2021-01-01 22:47:46 -0600

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -16,7 +16,7 @@ const Header = ({ data }) => (
           <div className="about-details">
             <span className="name">My Name is {data.name}.</span>
             <h2 className="sub-position">
-              I'm a Professional {data.designation}.
+              I'm a Professional Photographer.
             </h2>
             <div
               dangerouslySetInnerHTML={{
